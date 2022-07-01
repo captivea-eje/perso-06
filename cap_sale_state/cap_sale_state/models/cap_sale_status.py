28d6a728a4ac59e29b71b03bc02cb8f06c817ce5
Eva Jessu <79305136+captivea-eje@users.noreply.github.com>
2022-07-01 16:06:06 +0200

    cap_sale_state v2
    cap_sale_state v2

---
+++ cap_sale_state/cap_sale_state/models/cap_sale_status.py
@@ -0,0 +1,20 @@
+# -*- coding: utf-8 -*-
+
+from odoo import fields, models, api, _
+from odoo.exceptions import UserError
+import logging
+
+_logger = logging.getLogger(__name__)
+
+class CapSaleOrder(models.Model):
+    _inherit = "sale.order"
+	
+	state = fields.Selection([
+			('draft', 'Quotation'),
+			('sent', 'Quotation Sent'),
+			('accepted', 'Quotation accepted'),
+			('sale', 'Sales Order'),
+			('done', 'Locked'),
+			('cancel', 'Cancelled'),
+			], string='Status', readonly=True, copy=False, index=True, tracking=3, default='draft')
+		
