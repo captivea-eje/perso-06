b824e7c4029cb5c313564cb0804035c0402fcc06
Eva Jessu <79305136+captivea-eje@users.noreply.github.com>
2022-07-01 16:26:33 +0200

    Update cap_sale_status.py

--- cap_sale_state/models/cap_sale_status.py
+++ cap_sale_state/models/cap_sale_status.py
@@ -9,12 +9,11 @@ _logger = logging.getLogger(__name__)
 class CapSaleOrder(models.Model):
     _inherit = "sale.order"
 	
-	state = fields.Selection([
-			('draft', 'Quotation'),
-			('sent', 'Quotation Sent'),
-			('accepted', 'Quotation accepted'),
-			('sale', 'Sales Order'),
-			('done', 'Locked'),
-			('cancel', 'Cancelled'),
-			], string='Status', readonly=True, copy=False, index=True, tracking=3, default='draft')
-		
+    state = fields.Selection([
+		('draft', 'Quotation'),
+		('sent', 'Quotation Sent'),
+		('accepted', 'Quotation accepted'),
+		('sale', 'Sales Order'),
+		('done', 'Locked'),
+		('cancel', 'Cancelled'),
+		], string='Status', readonly=True, copy=False, index=True, tracking=3, default='draft')
